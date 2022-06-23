82f36a54600c4e8e5fd766652dbabe9145e51654
Vishakha Aggarwal <76397035+vishakha-aggarwal@users.noreply.github.com>
2022-06-24 00:17:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import io from "socket.io-client";
+import Header from './components/Header';
+import Container from './components/Container';
+import Footer from './components/Footer.jsx';
+import './style.css';
+
+const socket = io.connect();
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      <Header />
+      <Container socket = {socket} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
